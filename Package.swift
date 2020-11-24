ce06025d896d82f0d002ee00d74c6fddf0583c01
Kalaivani velusamy <kalaivanivelusamy@users.noreply.github.com>
2020-11-24 13:03:51 +0530

    Update Package.swift
    updated package

--- Package.swift
+++ Package.swift
@@ -22,6 +22,7 @@ let package = Package(
         .executable(name: "NIOTLSServer", targets: ["NIOTLSServer"]),
     ],
     dependencies: [
+    .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
     .package(url: "https://github.com/apple/swift-nio-ssl-support.git", from: "1.0.0"),
     ],
     targets: [
